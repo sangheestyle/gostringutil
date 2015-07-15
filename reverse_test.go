e4054ebaa8f937dd1182fb7fd65c9565fffc9c15
Sanghee Kim <sangheestyle@gmail.com>
2015-07-15 17:09:22 -0600

    test reverse function

---
+++ reverse_test.go
@@ -0,0 +1,19 @@
+package gostringutil
+
+import "testing"
+
+func TestReverse(t *testing.T) {
+	cases := []struct {
+		in, want string
+	}{
+		{"Hello, world", "dlrow ,olleH"},
+		{"Hello, 세계", "계세 ,olleH"},
+		{"", ""},
+	}
+	for _, c := range cases {
+		got := Reverse(c.in)
+		if got != c.want {
+			t.Errorf("Reverse(%q) == %q", c.in, got, c.want)
+		}
+	}
+}
